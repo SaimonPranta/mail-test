e729ab3dd5855534f369e81b7259ab5c57e7559c
pranta <you@example.com>
2024-09-22 14:54:07 +0600

    Commited at Time:-->Sun, Sep 22, 2024  2:54:06 PM

--- test.js
+++ test.js
@@ -37,7 +37,7 @@ qhEx26LoSfNddHXAiQJAcRARuSlPBjxNjjjwbGMite9vuDLtyEuQKKb54ylDd1mJ
 
 // Email options
 const mailOptions = {
-    from: "symul@micple.com", // Replace with your sender email
+    from: "symulkabirpranta@gmail.com", // Replace with your sender email
     to: "symulkabirpranta@gmail.com", // Recipient's email
     subject: "Backup Notification 10", // Subject line
     text: "This is a backup notification email sent from my custom SMTP server.", // Plain text body
